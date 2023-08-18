489fe5dc31ee752c152f793aa81b37177d38b2a4
maximilianstan <maximilian.s@outlook.com>
2023-08-18 17:30:01 +0300

    altele

--- testediverse.py
+++ testediverse.py
@@ -2,3 +2,23 @@ i = 3
 while i < 100:
     # do_something()
     i += 2
+s = "js"  # Hello in Greek
+print(s)
+
+for i in range(2, 8):
+    print("The value of i is currently", i)
+
+for i in range(2, 10, 3):
+    print("The value of i is currently", i)
+import time
+
+# Write a for loop that counts to five.
+    # Body of the loop - print the loop iteration number and the word "Mississippi".
+    # Body of the loop - use: time.sleep(1)
+
+# Write a print function with the final message.
+
+for a in range(1,6):
+    print(a,"Mississippi")
+    time.sleep(1)
+print("Ready or not, here I come!")
