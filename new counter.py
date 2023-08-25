5bd44187b42eb4b3a83fbdf1990e40536b1d2503
maximilianstan <maximilian.s@outlook.com>
2023-08-26 01:20:51 +0300

    Create new counter.py
    margarine nebune

---
+++ new counter.py
@@ -0,0 +1,11 @@
+
+numbers = [1, 2, 3, 4, 5]
+numbers.insert(0, -1)
+print(numbers)
+
+numbers = [1, 2, 3, 4, 5, 78, 343, 18, 22, 152]
+print(len(numbers))
+
+numbers = [1, 2, 3, 4, 5]
+for item in numbers:
+    print("margarine nebune")
