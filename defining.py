557c81da17be4ae1f8a0b18031abc897692e1cf9
maximilianstan <maximilian.s@outlook.com>
2023-09-25 20:34:14 +0300

    fresh

---
+++ defining.py
@@ -0,0 +1,28 @@
+print("We start here.")
+
+
+def message():
+    print("Enter a value: ")
+
+message()
+
+print("We end here.")
+
+def message():
+    print("Enter a value: ")
+
+message()
+a = int(input())
+message()
+b = int(input())
+message()
+c = int(input())
+
+def message():    # defining a function
+    print("Hello")    # body of the function
+
+message()    # calling the function
+message()
+message()
+message()
+message()
