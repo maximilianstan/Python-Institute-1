59010c7c1520840e2b1539d6557c546b3e8721c5
maximilianstan <maximilian.s@outlook.com>
2023-10-05 00:26:47 +0300

    fib

--- pythagoreantriangleverifierimproved.py
+++ pythagoreantriangleverifierimproved.py
@@ -26,3 +26,21 @@ def is_a_right_triangle(a, b, c):
 
 print(is_a_right_triangle(5, 3, 4))
 print(is_a_right_triangle(1, 3, 4))
+
+def is_a_triangle(a, b, c):
+    return a + b > c and b + c > a and c + a > b
+
+
+def heron(a, b, c):
+    p = (a + b + c) / 2
+    return (p * (p - a) * (p - b) * (p - c)) ** 0.5
+
+
+def area_of_triangle(a, b, c):
+    if not is_a_triangle(a, b, c):
+        return None
+    return heron(a, b, c)
+
+
+print(area_of_triangle(1., 1., 2. ** .5))
+
