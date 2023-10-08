4e8c375e568cc17b0bb324116c8a918b1f8bf95b
maximilianstan <maximilian.s@outlook.com>
2023-10-08 23:02:11 +0300

    Create tuples.py

---
+++ tuples.py
@@ -0,0 +1,15 @@
+tuple_1 = (1, 2, 4, 8)
+tuple_2 = 1., .5, .25, .125
+
+print(tuple_1)
+print(tuple_2)
+
+my_tuple = (1, 10, 100, 1000)
+
+print(my_tuple[0])
+print(my_tuple[-1])
+print(my_tuple[1:])
+print(my_tuple[:-2])
+
+for elem in my_tuple:
+    print(elem)
