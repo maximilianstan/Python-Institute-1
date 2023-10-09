f8ca7fc5dbe2477beecb10da728e658499faee50
maximilianstan <maximilian.s@outlook.com>
2023-10-09 23:38:46 +0300

    Create usingtuples_continued.py

---
+++ usingtuples_continued.py
@@ -0,0 +1,20 @@
+my_tuple = (1, 10, 100)
+
+t1 = my_tuple + (1000, 10000)
+t2 = my_tuple * 3
+
+print(len(t2))
+print(t1)
+print(t2)
+print(10 in my_tuple)
+print(-10 not in my_tuple)
+
+var = 123
+
+t1 = (1, )
+t2 = (2, )
+t3 = (3, var)
+
+t1, t2, t3 = t2, t3, t1
+
+print(t1, t2, t3)
