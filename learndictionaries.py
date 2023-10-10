da6baf46334096879c847a7a23c53c54ceb067b0
maximilianstan <maximilian.s@outlook.com>
2023-10-10 22:25:46 +0300

    Create learndictionaries.py

---
+++ learndictionaries.py
@@ -0,0 +1,7 @@
+dictionary = {"cat": "chat", "dog": "chien", "horse": "cheval"}
+phone_numbers = {'boss': 5551234567, 'Suzy': 22657854310}
+empty_dictionary = {}
+
+print(dictionary)
+print(phone_numbers)
+print(empty_dictionary)
