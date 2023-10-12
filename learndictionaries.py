10e60b25c21f521a8ae858b26b0b9552025455ca
maximilianstan <maximilian.s@outlook.com>
2023-10-12 15:59:27 +0300

    Update learndictionaries.py

--- learndictionaries.py
+++ learndictionaries.py
@@ -5,3 +5,17 @@ empty_dictionary = {}
 print(dictionary)
 print(phone_numbers)
 print(empty_dictionary)
+
+print(dictionary['cat'])
+print(phone_numbers['Suzy'])
+
+dictionary = {"cat": "chat", "dog": "chien", "horse": "cheval"}
+words = ['cat', 'lion', 'horse']
+
+for word in words:
+    if word in dictionary:
+        print(word, "->", dictionary[word])
+    else:
+        print(word, "is not in dictionary")
+
+
