f9d765a2a8c7e1d1ad658c9e1711e0df9aa9aab8
maximilianstan <maximilian.s@outlook.com>
2023-10-13 23:19:37 +0300

    catalog

--- learndictionaries.py
+++ learndictionaries.py
@@ -31,3 +31,13 @@ dictionary = {"cat": "chat", "dog": "chien", "horse": "cheval"}
 
 for english, french in dictionary.items():
     print(english, "->", french)
+
+dictionary = {"cat": "chat", "dog": "chien", "horse": "cheval"}
+
+for french in dictionary.values():
+    print(french)
+
+dictionary = {"cat": "chat", "dog": "chien", "horse": "cheval"}
+
+dictionary["cat"] = "minou"
+print(dictionary)
