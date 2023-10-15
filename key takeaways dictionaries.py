0ad07220cbcd9b4347c20c29a649df41d285670a
maximilianstan <maximilian.s@outlook.com>
2023-10-16 00:35:45 +0300

    key takeaways

---
+++ key takeaways dictionaries.py
@@ -0,0 +1,99 @@
+
+pol_eng_dictionary = {
+    "kwiat": "flower",
+    "woda": "water",
+    "gleba": "soil"
+    }
+
+item_1 = pol_eng_dictionary["gleba"]    # ex. 1
+print(item_1)    # outputs: soil
+
+item_2 = pol_eng_dictionary.get("woda")
+print(item_2)    # outputs: water
+
+pol_eng_dictionary = {
+    "zamek": "castle",
+    "woda": "water",
+    "gleba": "soil"
+    }
+
+pol_eng_dictionary["zamek"] = "lock"
+item = pol_eng_dictionary["zamek"]
+print(item)  # outputs: lock
+
+# To add or remove a key (and the associated value), use the following syntax:
+
+phonebook = {}    # an empty dictionary
+
+phonebook["Adam"] = 3456783958    # create/add a key-value pair
+print(phonebook)    # outputs: {'Adam': 3456783958}
+
+del phonebook["Adam"]
+print(phonebook)    # outputs: {}
+
+# update an item in a dictionary
+
+pol_eng_dictionary = {"kwiat": "flower"}
+
+pol_eng_dictionary.update({"gleba": "soil"})
+print(pol_eng_dictionary)    # outputs: {'kwiat': 'flower', 'gleba': 'soil'}
+
+pol_eng_dictionary.popitem()
+print(pol_eng_dictionary)    # outputs: {'kwiat': 'flower'}
+
+pol_eng_dictionary = {
+    "zamek": "castle",
+    "woda": "water",
+    "gleba": "soil"
+    }
+
+for item in pol_eng_dictionary:
+    print(item)
+
+# outputs: zamek
+#          woda
+#          gleba
+
+pol_eng_dictionary = {
+    "zamek": "castle",
+    "woda": "water",
+    "gleba": "soil"
+    }
+
+for key, value in pol_eng_dictionary.items():
+    print("Pol/Eng ->", key, ":", value)
+
+
+pol_eng_dictionary = {
+    "zamek": "castle",
+    "woda": "water",
+    "gleba": "soil"
+    }
+
+if "zamek" in pol_eng_dictionary:
+    print("Yes")
+else:
+    print("No")
+
+pol_eng_dictionary = {
+    "zamek": "castle",
+    "woda": "water",
+    "gleba": "soil"
+    }
+
+print(len(pol_eng_dictionary))    # outputs: 3
+del pol_eng_dictionary["zamek"]    # remove an item
+print(len(pol_eng_dictionary))    # outputs: 2
+
+pol_eng_dictionary.clear()   # removes all the items
+print(len(pol_eng_dictionary))    # outputs: 0
+
+del pol_eng_dictionary    # removes the dictionary
+
+pol_eng_dictionary = {
+    "zamek": "castle",
+    "woda": "water",
+    "gleba": "soil"
+    }
+
+copy_dictionary = pol_eng_dictionary.copy()
--- takeawaystuples_lists.py
+++ takeawaystuples_lists.py
@@ -44,3 +44,19 @@ my_tuple = 1, 2, 3,
 del my_tuple
 # print(my_tuple)    NameError: name 'my_tuple' is not defined
 
+# You can also create a tuple using a Python built-in function called tuple().
+# This is particularly useful when you want to convert a certain iterable (e.g., a list, range, string, etc.) to a tuple:
+
+my_tuple = tuple((1, 2, "string"))
+print(my_tuple)
+
+my_list = [2, 4, 6]
+print(my_list)    # outputs: [2, 4, 6]
+print(type(my_list))    # outputs: <class 'list'>
+tup = tuple(my_list)
+print(tup)    # outputs: (2, 4, 6)
+print(type(tup))    # outputs: <class 'tuple'>
+
+tup = 1, 2, 3,
+my_list = list(tup)
+print(type(my_list))    # outputs: <class 'list'>
